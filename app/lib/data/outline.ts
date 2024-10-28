d02ce2b23602efa12920a35151d1ef1bdcbf0b8f
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 21:28:40 +0000

    add content

--- app/lib/data/outline.ts
+++ app/lib/data/outline.ts
@@ -1,1 +1,18 @@
-export const outlineClasses = new Map<string, string>([["", ""]]);
+export const outlineClasses = new Map<string, string>([
+  // Outline Width
+  ["outline-0", "ow-0"],
+  ["outline-1", "ow-1"],
+  ["outline-2", "ow-2"],
+  ["outline-4", "ow-4"],
+
+  // Outline Style
+  ["outline-none", "os-none"],
+  ["outline", "os-s"],
+  ["outline-dashed", "os-d"],
+
+  // Outline Offset
+  ["outline-offset-0", "oo-0"],
+  ["outline-offset-1", "oo-1"],
+  ["outline-offset-2", "oo-2"],
+  ["outline-offset-4", "oo-4"],
+]);
