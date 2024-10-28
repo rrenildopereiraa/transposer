e83cf67354cfc24894976423f0b341a55b8e525e
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 21:07:00 +0000

    add content

--- app/lib/data/typography.ts
+++ app/lib/data/typography.ts
@@ -1,1 +1,74 @@
-export const typographyClasses = new Map<string, string>([["", ""]]);
+export const typographyClasses = new Map<string, string>([
+  // Font Family
+  ["font-sans", "ff-s"],
+  ["font-serif", "ff-c"],
+  ["font-mono", "ff-m"],
+
+  // Font Size
+  ["text-xs", "fs-xs"],
+  ["text-sm", "fs-b"],
+  ["text-base", "fs-b"],
+  ["text-lg", "fs-sm"],
+  ["text-xl", "fs-md"],
+  ["text-2xl", "fs-lg"],
+  ["text-3xl", "fs-xl"],
+  ["text-4xl", "fs-xxl"],
+  ["text-5xl", "fs-3xl"],
+  ["text-6xl", "fs-6xl"],
+
+  // Font Style
+  ["italic", "fs-i"],
+  ["not-italic", "fs-n"],
+
+  // Font Weight
+  ["font-thin", "fw-100"],
+  ["font-extralight", "fw-200"],
+  ["font-light", "fw-300"],
+  ["font-normal", "fw-400"],
+  ["font-medium", "fw-500"],
+  ["font-semibold", "fw-600"],
+  ["font-bold", "fw-700"],
+  ["font-extrabold", "fw-800"],
+  ["font-black", "fw-900"],
+
+  // Line Height
+  ["leading-6", "lh-4"],
+  ["leading-8", "lh-6"],
+  ["leading-none", "lh-1"],
+  ["leading-tight", "lh-2"],
+  ["leading-snug", "lh-3"],
+  ["leading-normal", "lh-4"],
+  ["leading-relaxed", "lh-5"],
+  ["leading-loose", "lh-6"],
+
+  // List Style Type
+  ["list-disc", "lst-d"],
+
+  // Text Align
+  ["text-left", "ta-l"],
+  ["text-center", "ta-c"],
+  ["text-right", "ta-r"],
+  ["text-justify", "ta-j"],
+  ["text-start", "ta-s"],
+  ["text-end", "ta-e"],
+
+  // Text Decoration
+  ["underline", "td-u"],
+  ["no-underline", "td-none"],
+
+  // Text Decoration Style
+  ["decoration-solid", "tds-s"],
+  ["decoration-dashed", "tds-d"],
+
+  // Text Decoration Thickness
+  ["decoration-auto", "tdt-auto"],
+  ["decoration-from-font", "tdt-ff"],
+  ["decoration-0", "tdt-0"],
+  ["decoration-1", "tdt-1"],
+  ["decoration-2", "tdt-2"],
+  ["decoration-4", "tdt-4"],
+
+  // Word Break
+  ["break-normal", "ow-n"],
+  ["break-words", "ow-bw"],
+]);
