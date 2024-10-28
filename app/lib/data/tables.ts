fd6b7bf1b5c3362308a1858edb211997b99e25bb
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 22:44:26 +0000

    add content

--- app/lib/data/tables.ts
+++ app/lib/data/tables.ts
@@ -1,1 +1,9 @@
-export const tableClasses = new Map<string, string>([["", ""]]);
+export const tableClasses = new Map<string, string>([
+  // Caption Side
+  ["caption-top", "cs-t"],
+  ["caption-bottom", "cs-b"],
+
+  // Table Layout
+  ["table-auto", "tl-auto"],
+  ["table-fixed", "tl-f"],
+]);
