146b4445b040b3b8245b05c8a98821a6713fa07b
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 22:27:59 +0000

    add content

--- app/lib/data/effects.ts
+++ app/lib/data/effects.ts
@@ -6,4 +6,17 @@ export const effectClasses = new Map<string, string>([
   ["shadow-md", "bs-md"],
   ["shadow-lg", "bs-lg"],
   ["shadow-xl", "bs-xl"],
+
+  // Opacity
+  ["opacity-0", "o-0"],
+  ["opacity-10", "o-10"],
+  ["opacity-20", "o-20"],
+  ["opacity-30", "o-30"],
+  ["opacity-40", "o-40"],
+  ["opacity-50", "o-50"],
+  ["opacity-60", "o-60"],
+  ["opacity-70", "o-70"],
+  ["opacity-80", "o-80"],
+  ["opacity-90", "o-90"],
+  ["opacity-100", "o-100"],
 ]);
