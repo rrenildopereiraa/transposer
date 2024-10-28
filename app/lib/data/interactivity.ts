a4b93ce1cf1d50eb06cf08ac3e461eab03a72c2b
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 22:41:05 +0000

    add content

--- app/lib/data/interactivity.ts
+++ app/lib/data/interactivity.ts
@@ -1,1 +1,48 @@
-export const interactivityClasses = new Map<string, string>([["", ""]]);
+export const interactivityClasses = new Map<string, string>([
+  // Appearance
+  ["appearance-none", "a-none"],
+  ["appearance-auto", "a-auto"],
+
+  // Cursor
+  ["cursor-auto", "c-auto"],
+  ["cursor-default", "c-d"],
+  ["cursor-pointer", "c-p"],
+  ["cursor-wait", "c-w"],
+  ["cursor-text", "c-t"],
+  ["cursor-move", "c-m"],
+  ["cursor-help", "c-h"],
+  ["cursor-not-allowed", "c-na"],
+  ["cursor-none", "c-none"],
+  ["cursor-progress", "c-pr"],
+  ["cursor-crosshair", "c-ch"],
+  ["cursor-col-resize", "c-cr"],
+  ["cursor-row-resize", "c-rs"],
+  ["cursor-n-resize", "c-nr"],
+  ["cursor-e-resize", "c-wr"],
+  ["cursor-s-resize", "c-sr"],
+  ["cursor-w-resize", "c-wr"],
+  ["cursor-ne-resize", "c-ner"],
+  ["cursor-nw-resize", "c-nwr"],
+  ["cursor-se-resize", "c-ser"],
+  ["cursor-sw-resize", "c-swr"],
+  ["cursor-nesw-resize", "c-neswr"],
+  ["cursor-nwse-resize", "c-nwser"],
+  ["cursor-zoom-in", "c-zi"],
+  ["cursor-zoom-out", "c-zo"],
+
+  // Pointer Events
+  ["pointer-events-none", "pe-none"],
+  ["pointer-events-auto", "pe-auto"],
+
+  // Resize
+  ["resize-none", "r-none"],
+  ["resize-y", "r-v"],
+  ["resize-x", "r-h"],
+  ["resize", "r-b"],
+
+  // User Select
+  ["select-none", "us-none"],
+  ["select-text", "us-t"],
+  ["select-all", "us-a"],
+  ["select-auto", "us-auto"],
+]);
