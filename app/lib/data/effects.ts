8ca333b03da4e3b97f26a37aa6671475698d4424
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 22:27:01 +0000

    add content

--- app/lib/data/effects.ts
+++ app/lib/data/effects.ts
@@ -1,1 +1,9 @@
-export const effectClasses = new Map<string, string>([["", ""]]);
+export const effectClasses = new Map<string, string>([
+  // Box Shadow
+  ["shadow-none", "bs-none"],
+  ["shadow-sm", "bs-xs"],
+  ["shadow", "bs-sm"],
+  ["shadow-md", "bs-md"],
+  ["shadow-lg", "bs-lg"],
+  ["shadow-xl", "bs-xl"],
+]);
