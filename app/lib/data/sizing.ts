adc9e498fece46073071fc3ef909406abd803890
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 12:48:08 +0000

    add content

--- app/lib/data/sizing.ts
+++ app/lib/data/sizing.ts
@@ -1,1 +1,60 @@
-export const sizingClasses = new Map<string, string>([["", ""]]);
+export const sizingClasses = new Map<string, string>([
+  // Width
+  ["w-auto", "w-fc"],
+  ["w-1/2", "w-half"],
+  ["w-2/4", "w-half"],
+  ["w-3/6", "w-half"],
+  ["w-6/12", "w-half"],
+  ["w-full", "w-full"],
+  ["w-dvw", "w-1/1"],
+  ["w-min", "w-min"],
+  ["w-max", "w-max"],
+  ["w-fit", "w-fc"],
+
+  // Min-Width
+  ["min-w-full", "min-w-full"],
+  ["min-w-min", "min-w-min"],
+  ["min-w-max", "min-w-max"],
+  ["min-w-fit", "min-w-fc"],
+
+  // Max-Width
+  ["max-w-full", "max-w-full"],
+  ["max-w-min", "max-w-min"],
+  ["max-w-max", "max-w-max"],
+  ["max-w-fit", "max-w-fc"],
+
+  // Height
+  ["h-1/2", "h-half"],
+  ["h-2/4", "h-half"],
+  ["h-3/6", "h-half"],
+  ["h-full", "h-full"],
+  ["h-dvh", "h-1/1"],
+  ["h-min", "h-min"],
+  ["h-max", "h-max"],
+  ["h-fit", "h-fc"],
+
+  // Min-Height
+  ["min-h-full", "min-h-full"],
+  ["min-h-dvh", "min-h-1/1"],
+  ["min-h-min", "min-h-min"],
+  ["min-h-max", "min-h-max"],
+  ["min-h-fit", "min-h-fc"],
+
+  // Max-Height
+  ["max-h-full", "max-h-full"],
+  ["max-h-dvh", "max-h-1/1"],
+  ["max-h-min", "max-h-min"],
+  ["max-h-max", "max-h-max"],
+  ["max-h-fit", "max-h-fc"],
+  ["max-h-1/2", "max-h-1/2"],
+
+  // Size
+  ["size-1/2", "dim-half"],
+  ["size-2/4", "dim-half"],
+  ["size-3/6", "dim-half"],
+  ["size-6/12", "dim-half"],
+  ["size-full", "dim-full"],
+  ["size-min", "dim-min"],
+  ["size-max", "dim-max"],
+  ["size-fit", "dim-fc"],
+]);
