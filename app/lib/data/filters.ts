f589f410d7332c84f6473d3e6f68520da44bed71
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-28 22:27:16 +0000

    add content

--- app/lib/data/filters.ts
+++ app/lib/data/filters.ts
@@ -1,1 +1,9 @@
-export const filterClasses = new Map<string, string>([["", ""]]);
+export const filterClasses = new Map<string, string>([
+  // Blur
+  ["blur-none", "bf-b-none"],
+  ["blur-sm", "bf-b-xs"],
+  ["blur", "bf-b-sm"],
+  ["blur-md", "bf-b-md"],
+  ["blur-lg", "bf-b-lg"],
+  ["blur-xl", "bf-b-xl"],
+]);
