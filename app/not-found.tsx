8b01d41f5bcdf263bd78eb99d4b07e2a668470bd
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-01 15:48:44 +0000

    add `not-found.tsx` page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <section className="d-f jc-c lg:mx-56 min-h-1/1 mx-10">
+      <div className="mt-12 ta-c">
+        <h1 className=" fs-md fw-700 tc-white">404</h1>
+        <p className="fs-sm fw-400 tc-white">This page is not real.</p>
+      </div>
+    </section>
+  );
+}
