b4d69350a3a488b155856d32ca16c94ba14eec80
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-01 15:53:37 +0000

    update `not-found.tsx` page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <section className="d-f jc-c lg:mx-56 min-h-1/1 mx-10">
-      <div className="mt-12 ta-c">
+    <section className="ins lg:mx-56 min-h-1/1 mx-10">
+      <div className="ta-c">
         <h1 className=" fs-md fw-700 tc-white">404</h1>
         <p className="fs-sm fw-400 tc-white">This page is not real.</p>
+        <p className="bg-white fw-600 h:bg-l-silver-6 mt-6 px-4 py-2 rad-1 tc-black w-100">
+          <Link href="/">Go back home</Link>
+        </p>
       </div>
     </section>
   );
