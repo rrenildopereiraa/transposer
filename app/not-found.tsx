a7ee7dde74590ecb61ae16d10b5c089f698cd5d5
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-13 13:54:05 +0000

    refactor: remove Docs component and update NotFound styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <section className="ins lg:mx-56 min-h-1/1 mx-10">
+    <section className="ai-c d-f jc-c lg:mx-56 min-h-1/1 mx-10">
       <div className="ta-c">
-        <h1 className="fs-md fw-700 tc-white">404</h1>
-        <p className="fs-sm fw-400 tc-white">This page is not real.</p>
-        <p className="bg-white fw-600 h:bg-l-silver-6 mt-6 px-4 py-2 rad-1 tc-black w-100">
+        <h1 className="fs-lg fw-700 tc-white">404</h1>
+        <p className="fs-sm fw-300 tc-white">This page could not be found.</p>
+        <p className="b-1 bc-d-lead-4 bg-d-lead-6 my-4 px-5 py-2 rad-1 s-x-6 tc-silver">
           <Link href="/">Go back home</Link>
         </p>
       </div>
